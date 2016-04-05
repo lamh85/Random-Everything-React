a56765042e0eceb31d26866831c7623850e9c955
Hon Lam <lamh85@gmail.com>
2016-04-04 20:21:25 -0700

    Built the style object factory.

--- javascript/components/Application.jsx
+++ javascript/components/Application.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Shape from './Shape'
 
-export default class Application extends React.Component {
+export default class extends React.Component {
   render(){
+
+    const makeRandom = (multiplier = 1, floor = 0) => Math.random() * multiplier + floor;
+
+    const makeStyle = () => {
+      return {
+        backgroundColor: `rgb(${Math.floor(makeRandom(255))}, ${Math.floor(makeRandom(255))}, ${Math.floor(makeRandom(255))})`,
+        top: `${makeRandom(100)}%`,
+        left: `${makeRandom(100)}%`,
+        opacity: makeRandom(),
+        transform: `rotate(${makeRandom(360)}deg)`,
+        width: `${makeRandom(300, 100)}px`,
+        height: `${makeRandom(200, 100)}px`
+      }
+    }
+
     return (
       <div>
-        I am from component
+        {JSON.stringify(makeStyle())}
+        <Shape styleProp = {makeStyle()} />
       </div>
     )
   }
---
+++ javascript/components/Shape.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class extends React.Component {
+  render(){
+    return (
+      <div style={this.props.styleProp} className="shape">
+      </div>
+    )
+  }
+}
