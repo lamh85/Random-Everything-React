71348ceadd24c89f56ffa2731c1d6fda1996fd2f
Hon Lam <lamh85@gmail.com>
2016-04-06 19:15:37 -0700

    Created end values for styles for shape.

--- javascript/components/Application.jsx
+++ javascript/components/Application.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Shape from './Shape'
 
 export default class extends React.Component {
+
+
   render(){
 
     const makeRandom = (multiplier = 1, floor = 0) => Math.random() * multiplier + floor;
@@ -21,7 +23,18 @@ export default class extends React.Component {
     return (
       <div>
         {JSON.stringify(makeStyle())}
-        <Shape styleProp = {makeStyle()} />
+        <Shape
+          styleProp={makeStyle()}
+          endColorR={Math.floor(makeRandom(255))}
+          endColorG={Math.floor(makeRandom(255))}
+          endColorB={Math.floor(makeRandom(255))}
+          endTop={makeRandom(100)}
+          endLeft={makeRandom(100)}
+          endOpacity={makeRandom()}
+          endRotation={makeRandom(360)}
+          endWidth={makeRandom(300, 100)}
+          endHeight={makeRandom(300, 100)}
+        />
       </div>
     )
   }
--- javascript/components/Shape.jsx
+++ javascript/components/Shape.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default class extends React.Component {
+  componentDidMount(){
+    console.log(this.refs.shape.style);
+  }
   render(){
     return (
-      <div style={this.props.styleProp} className="shape">
+      <div style={this.props.styleProp} className="shape" ref="shape">
       </div>
     )
   }
