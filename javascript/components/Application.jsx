f6bdfc8b15afcdb47fbb966898ce6d59041ee74b
Hon Lam <lamh85@gmail.com>
2016-04-11 21:08:53 -0700

    Learning promise object.

--- javascript/components/Application.jsx
+++ javascript/components/Application.jsx
@@ -4,6 +4,18 @@ import Shape from './Shape';
 
 export default class extends React.Component {
 
+  componentWillMount(){
+    var promise = new Promise(function(resolve, reject) {
+      // do a thing, possibly async, then…
+
+      if (/* everything turned out fine */) {
+        resolve("Stuff worked!");
+      }
+      else {
+        reject(Error("It broke"));
+      }
+    });
+  }
 
   render(){
 
