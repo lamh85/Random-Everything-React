10a4362768db6c1f2b2286b0e063f71e01e7ef8b
Hon Lam <lamh85@gmail.com>
2016-04-23 15:21:03 -0700

    Started coding the transition of each CSS property.

--- javascript/components/Application.jsx
+++ javascript/components/Application.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import _ from 'lodash';
 import Shape from './Shape';
 
+const DURATION_PER_FRAME = 100;
+
 class Application extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      shapeStyles: []
+      startStyles: [],
+      currentStyles: [],
+      endStyles: []
     }
   }
 
@@ -16,12 +20,64 @@ class Application extends React.Component {
   }
 
   stylePusher(){
+    /*
+    */
     setInterval(() =>{
-      var shapesArray = this.state.shapeStyles;
-      var newStyle = this.styleFactory();
-      shapesArray.push(newStyle);
-      this.setState({shapeStyles: shapesArray});
+      var startStyles = this.state.startStyles;
+      var newStartStyle = this.styleFactory();
+      startStyles.push(newStartStyle);
+      this.setState({startStyles: startStyles});
+
+      var currentStyles = this.state.currentStyles;
+      var newCurrentStyle = this.styleFactory();
+      newCurrentStyle.currentTime = 0;
+      newCurrentStyle.currentFrame = 0;
+      currentStyles.push(newCurrentStyle);
+      this.setState({currentStyles: currentStyles});
+
+      var endStyles = this.state.endStyles;
+      var newEndStyle = this.styleFactory();
+      newEndStyle.duration = this.makeRandom(3, 5, 1000);
+      endStyles.push(newEndStyle);
+      this.setState({endStyles: endStyles});
     }, 250);
+      /*
+    */
+  }
+
+  styleChanger(){
+    var currentStyles = this.state.currentStyles;
+    // Loop through array of styles
+    currentStyles.map((style, index) => {
+      // var keys = _.keys(style).map(element => element != "backgroundColor");
+      var styleObject = {};
+      var currentFrame = style.currentFrame;
+      styleObject.red = this.currentFrameAttribute("red", index, currentFrame);
+      styleObject.green = this.currentFrameAttribute("green", index, currentFrame);
+      styleObject.blue = this.currentFrameAttribute("blue", index, currentFrame);
+      styleObject.top = this.currentFrameAttribute("top", index, currentFrame, false) + "%";
+      styleObject.left = this.currentFrameAttribute("left", index, currentFrame, false) + "%";
+      styleObject.opacity = this.currentFrameAttribute("opacity", index, currentFrame, false);
+      styleObject.transform = this.currentFrameAttribute("");
+      // transform: `rotate(${this.makeRandom(0, 360)}deg)`,
+      styleObject.width = this.currentFrameAttribute("width", index, currentFrame) + "px";
+      // keys.map(cssProp => )
+      // return style object
+    });
+  }
+
+  currentFrameAttribute(attribute, arrayIndex, currentFrame, round = true){
+    var startValue = this.state.startStyles[arrayIndex].attribute;
+    var endValue = this.state.endStyles[arrayIndex].attribute;
+    var valuePerFrame = (parseFloat(endValue) - parseFloat(startValue)) / DURATION_PER_FRAME;
+    var currentValue = startValue + valuePerFrame * currentFrame;
+    return round ? _.round(currentValue) : currentValue;
+  }
+
+  styleChangePerFrame(attribute, arrayIndex){
+    var startValue = this.state.startStyles[arrayIndex].attribute;
+    var endValue = this.state.endStyles[arrayIndex].attribute;
+    return (endValue - startValue) / DURATION_PER_FRAME;
   }
 
   makeRandom(floor = 0, ceiling = 0, multiplier = 1){
@@ -37,14 +93,22 @@ class Application extends React.Component {
   }
 
   styleFactory(){
+    var red = this.makeRandom255();
+    var green = this.makeRandom255();
+    var blue = this.makeRandom255();
+
     return {
-      backgroundColor: `rgb(${this.makeRandom255()}, ${this.makeRandom255()}, ${this.makeRandom255()})`,
+      red: red,
+      green: green,
+      blue: blue,
+      backgroundColor: `rgb(${red}, ${green}, ${blue})`,
       top: `${this.makeRandom100()}%`,
       left: `${this.makeRandom100()}%`,
-      opacity: this.makeRandom(40, 100, 0.01),
+      opacity: this.makeRandom(10, 100, 0.01),
       transform: `rotate(${this.makeRandom(0, 360)}deg)`,
       width: `${this.makeRandom(100, 400)}px`,
       height: `${this.makeRandom(100, 300)}px`,
+      borderRadius: `${this.makeRandom(0, 50)}%`,
       position: "absolute"
     }
   }
@@ -53,7 +117,7 @@ class Application extends React.Component {
 
     return (
       <div>
-        {this.state.shapeStyles.map((style, elementIndex)=> <div style={style} key={elementIndex}></div>)}
+        {this.state.currentStyles.map((style, elementIndex)=> <div style={style} key={elementIndex}></div>)}
       </div>
     )
   }
