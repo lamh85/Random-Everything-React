948ff34402999d423ca97c3ce579228d516ad856
Hon Lam <lamh85@gmail.com>
2016-04-25 19:02:42 -0700

    Building the function that changes the shape's style.

--- javascript/components/Application.jsx
+++ javascript/components/Application.jsx
@@ -58,7 +58,7 @@ class Application extends React.Component {
       styleObject.top = this.currentFrameAttribute("top", index, currentFrame, false) + "%";
       styleObject.left = this.currentFrameAttribute("left", index, currentFrame, false) + "%";
       styleObject.opacity = this.currentFrameAttribute("opacity", index, currentFrame, false);
-      styleObject.transform = this.currentFrameAttribute("") + "deg)";
+      styleObject.transform = "roate("+ this.currentFrameAttribute("") +"deg)";
       // transform: `rotate(${this.makeRandom(0, 360)}deg)`,
       styleObject.width = this.currentFrameAttribute("width", index, currentFrame) + "px";
       // keys.map(cssProp => )
